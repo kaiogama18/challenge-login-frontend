aeedfe67d23933d919c7658b3b96dca88b3bfc9c
Kaio Gama <kaioeduardoescar@gmail.com>
2021-02-13 07:56:44 -0400

    [Config] --> create folde src
    Signed-off-by: Kaio Gama <kaioeduardoescar@gmail.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,33 +1,35 @@
-import Document, { DocumentContext } from "next/document";
-import { ServerStyleSheet } from "styled-components";
+import Document, { DocumentContext, DocumentInitialProps } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
+    static async getInitialProps(
+        ctx: DocumentContext
+    ): Promise<DocumentInitialProps> {
+        const sheet = new ServerStyleSheet()
+        const originalRenderPage = ctx.renderPage
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        });
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: App => props =>
+                        sheet.collectStyles(<App {...props} />)
+                })
 
-      const initialProps = await Document.getInitialProps(ctx);
+            const initialProps = await Document.getInitialProps(ctx)
 
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      };
-    } finally {
-      sheet.seal();
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                )
+            }
+        } finally {
+            sheet.seal()
+        }
     }
-  }
 }
 
-export default MyDocument;
+export default MyDocument

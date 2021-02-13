ea7acd6716b90a27041e08ce17a27e5a589142e8
Kaio Gama <kaioeduardoescar@gmail.com>
2021-02-13 07:50:20 -0400

    [Prettier] -- Create prettier and eslint
    Signed-off-by: Kaio Gama <kaioeduardoescar@gmail.com>

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main>
+                <h1>testsssss</h1>
+            </main>
+        </div>
+    )
+}
+
+export default Home

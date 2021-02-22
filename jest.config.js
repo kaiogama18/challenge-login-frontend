28dc33b94d03af98b30eafd668cc158f8b0234c9
Kaio Gama <kaioeduardoescar@gmail.com>
2021-02-21 20:46:43 -0400

    [text] --> npm run test
    Signed-off-by: Kaio Gama <kaioeduardoescar@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,29 @@
-
 module.exports = {
-    verbose: true
-}
+    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+    bail: 1,
+    clearMocks: true,
+    collectCoverage: true,
+    collectCoverageFrom: ['components/**/*.js', 'pages/**/*.js'],
+    coverageReporters: ['lcov', 'text'],
+  };
 
-module.exports = async () => {
-    return {
-        verbose: true
-    }
-}
+// module.exports = {
+//     verbose: true
+// }
 
-module.exports = {
-    transform: {
-      "^.+\\.js$": "babel-jest",
-      "^.+\\.svelte$": "svelte-jest"
-    },
-    moduleFileExtensions: ["js", "json", "svelte"],
-    coverageReporters: ['html'],
-    setupFilesAfterEnv: [
-        "@testing-library/jest-dom/extend-expect"
-    ],
-    preset: 'ts-jest'
+// module.exports = async () => {
+//     return {
+//         verbose: true
+//     }
+// }
 
-};
+// module.exports = {
+//     transform: {
+//         '^.+\\.js$': 'babel-jest',
+//         '^.+\\.svelte$': 'svelte-jest'
+//     },
+//     moduleFileExtensions: ['js', 'json', 'svelte'],
+//     coverageReporters: ['html'],
+//     setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+//     preset: 'ts-jest'
+// }
